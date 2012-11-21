97fcafea79981ef46dd6259c9978c23327f23fdc
Xabier Ochotorena <xabi@ochoto.com>
2012-11-21 23:36:54 +0100

    Mejoras en la generación del mapa
    Emparejamos con for comprehensions, filtramos td[colspan]

--- src/main/scala/com/github/ochoto/ClusterTicDb.scala
+++ src/main/scala/com/github/ochoto/ClusterTicDb.scala
@@ -13,41 +13,51 @@ import scala.collection.JavaConverters._
 object ClusterTicDb  {
 	def main(args:Array[String]): Unit = {
 		val dir = args(0)
-		val htmlFiles = new File(dir).listFiles.filter(_.getName.endsWith("*.html"))
+		val htmlFiles = new File(dir).listFiles.filter(_.getName.endsWith(".html"))
+		val fichas = htmlFiles map ( parseFile(_) ) 
 
-		htmlFiles map ( parseFile(_) ) map ( println(_) )
+		val fichasConDatos = fichas filter { case (t,l) => !t.isEmpty }
+		fichasConDatos map ( println(_) )
 	}
 
-	def parseFile(file: File) {
+	def parseFile(file: File) = {
 		println("Procesando: [" + file.getName + "]")
 		
 		val doc = Jsoup.parse(file, "UTF-8", "")
 		
 		val ficha = doc.select("div.fichas").first
 		val empresa = ficha.select("h1").first.text.trim
-		println("Encontrada empresa: " + empresa)
-
-		val tablas = ficha.select("table.FichaTabla").asScala
-		
-		val fichaParseada = for {
-			t <- tablas
+		if (empresa.isEmpty) {
+			println("El fichero [" + file.getName + "] esta vacío")
+			("",Nil)
 		}
-		yield processTable(t)
+		else {
+			println("Encontrada empresa: " + empresa)
 
-		println( (empresa,fichaParseada) )
+			val tablas = ficha.select("table.FichaTabla").asScala
+			val fichaParseada = for {
+				t <- tablas
+			}
+			yield processTable(t)
+
+			(empresa,fichaParseada)
+		}
 	}
 
 	//	Titulo, Lista de pares clave/valor
 	type Tabla = (String, Map[String,String])
 
 	def processTable(t: Element): Tabla = {
-		val tds = t.select("td")
-		val titulo = tds.first.text
-		val kvs = tds.asScala.drop(1).zipWithIndex
-		val ks = kvs.filter { case (e,i) => i % 2 == 0 } map (_._1.text)
-		val vs = kvs.filter { case (e,i) => i % 2 == 1 } map (_._1.text)
-		val kvMap = (ks zip vs).toMap
-		(titulo, kvMap)
+		val tds = t.select("td").asScala
+		val titulo = tds(0).text
+
+		val kv = for {
+			e <- tds drop(1) grouped(2)
+			if (e.size == 2)
+			p = (e(0).text, e(1).text)
+		} yield p
+
+		(titulo, kv.toMap)
 	}
 }
 
